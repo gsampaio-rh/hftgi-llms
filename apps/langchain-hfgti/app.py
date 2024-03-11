a5829b1e87fe7f2d0d48a9a8fa75c36bcbbbee54
Gabriel Sampaio <gsampaio@redhat.com>
2024-03-11 18:19:05 -0300

    kafka

--- apps/langchain-hfgti/app.py
+++ apps/langchain-hfgti/app.py
@@ -5,6 +5,27 @@ from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceTextGenInference
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.prompts import PromptTemplate
+from kafka import KafkaConsumer, KafkaProducer
+import json
+
+# Kafka setup
+kafka_server = "localhost:9092"  # Change this to your Kafka server address
+consumer_topic = "chat"
+producer_topic = "answer"
+
+consumer = KafkaConsumer(
+    consumer_topic,
+    bootstrap_servers=[kafka_server],
+    auto_offset_reset="earliest",
+    enable_auto_commit=True,
+    group_id="chat-group",
+    # value_deserializer=lambda x: json.loads(x.decode("utf-8")),
+)
+
+producer = KafkaProducer(
+    bootstrap_servers=[kafka_server],
+    value_serializer=lambda x: json.dumps(x).encode("utf-8"),
+)
 
 def read_conversation_file(file_path):
     """Reads content from a specified file path."""
@@ -12,7 +33,9 @@ def read_conversation_file(file_path):
         txt = file.read()
     return txt
 
-inference_server_url = "http://hf-tgi-server.llms.svc.cluster.local:3000/"
+# inference_server_url = "http://hf-tgi-server.llms.svc.cluster.local:3000/"
+
+inference_server_url = "http://localhost:3000/"
 
 template = """
 <s>[INST] <<SYS>>
@@ -41,13 +64,36 @@ llm = HuggingFaceTextGenInference(
     typical_p=0.95,
     temperature=0.1,
     repetition_penalty=1.175,
-    streaming=True,
-    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
 chain = QA_CHAIN_PROMPT | llm
 
-file_path = "sample_chat.txt"
-conversation_text = read_conversation_file(file_path)
+# file_path = "sample_chat.txt"
+# conversation_text = read_conversation_file(file_path)
 
-chain.invoke({"conversation": conversation_text})
+# answer = chain.invoke({"conversation": conversation_text})
+
+# print(answer)
+
+for message in consumer:
+    try:
+        # Decode the message value from bytes to a string and then load it as JSON
+        conversation_data = json.loads(message.value.decode('utf-8'))
+        conversation_text = conversation_data['conversation']
+
+        print("CHAT: ->", conversation_text)
+
+        # Process the conversation with the chain
+        response = chain.invoke({"conversation": conversation_text})
+
+        print("RESPOSTA: ->",response)
+
+        # Prepare the result to be sent to the 'answer' topic
+        result = {"conversation": conversation_text, "response": response}
+
+        # Send the processed result to the Kafka 'answer' topic
+        producer.send(producer_topic, value=result)
+        print("Processed and sent conversation to 'answer' topic.")
+
+    except Exception as e:
+        print(f"Error processing message: {e}")
