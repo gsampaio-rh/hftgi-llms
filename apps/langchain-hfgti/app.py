7f8d45a66231eeaf9bbb7be3c3ee4ae4bcda2872
Gabriel Sampaio <gsampaio@redhat.com>
2024-03-11 10:38:41 -0300

    The application now is a terminal chat

--- apps/langchain-hfgti/app.py
+++ apps/langchain-hfgti/app.py
@@ -1,8 +1,28 @@
 # Imports
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.vectorstores.pgvector import PGVector
+from langchain.chains import RetrievalQA
 from langchain.llms import HuggingFaceTextGenInference
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.prompts import PromptTemplate
 
 inference_server_url = "http://hf-tgi-server.llms.svc.cluster.local:3000/"
+
+template = """<s>[INST] <<SYS>>
+You are a helpful, respectful and honest assistant.
+You will be given a question you need to answer, and a context to provide you with information. You must answer the question based as much as possible on this context.
+Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information.
+<</SYS>>
+
+Question: {question}
+Context: {context} [/INST]
+"""
+
+QA_CHAIN_PROMPT = PromptTemplate.from_template(template)
+
+embeddings = HuggingFaceEmbeddings()
 
 # Basic llm object definition, no text streaming
 llm = HuggingFaceTextGenInference(
@@ -11,8 +31,27 @@ llm = HuggingFaceTextGenInference(
     top_k=10,
     top_p=0.95,
     typical_p=0.95,
-    temperature=0.01,
-    repetition_penalty=1.03,
+    temperature=0.1,
+    repetition_penalty=1.175,
+    streaming=True,
+    callbacks=[StreamingStdOutCallbackHandler()],
 )
 
-llm("Can you describe Paris in 100 words?")
+chain = QA_CHAIN_PROMPT | llm
+
+context = ""
+
+# question = "Can you describe Paris in 200 words?"
+# chain.invoke({"question": question, "context": context})
+
+# Interactive questions and answers
+while True:
+    query = input("\nEnter a query: ")
+    if query == "exit":
+        break
+    # Get the answer from the chain
+    chain.invoke({"question": query, "context": context})
+
+# llm_streaming(
+#     "Can you describe Paris in 200 words?", callbacks=[StreamingStdOutCallbackHandler()]
+# )
